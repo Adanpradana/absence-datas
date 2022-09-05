b49b5efa1c5d8659de31b5ff1104915b249951bd
Adanpradana <adanizkahariz@gmail.com>
2022-09-05 17:15:57 +0700

    show datatables from employee.json

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,3 +1,5 @@
+import { Children } from "react";
+import datas from "../data/employee.json";
 const Employee = () => {
   return (
     <section className="container">
@@ -5,7 +7,38 @@ const Employee = () => {
         <i className="bi bi-person-circle"></i>
       </div>
       <div className="container-content">
-        <h1>Employee</h1>
+        <div className="container-table">
+          <h3>Employee Details</h3>
+          <div className="table-wrapper">
+            <table>
+              <thead>
+                <tr>
+                  {datas.GRID.COLUMNS.COLUMN.map((response, i) => (
+                    <th key={i}>{response._attributes.Caption}</th>
+                  ))}
+                  <th>Action</th>
+                </tr>
+              </thead>
+              {Children.toArray(
+                datas.GRID.ROWS.ROW.map((data) => (
+                  <tbody>
+                    <tr>
+                      <td>{data._attributes.dbg_pegawaipegawai_nip}</td>
+                      <td>{data._attributes.dbg_pegawaipegawai_pin}</td>
+                      <td>{data._attributes.dbg_pegawaipegawai_nama}</td>
+                      <td>{data._attributes.dbg_pegawaijdw_kerja_m_name}</td>
+                      <td></td>
+                      <td>{data._attributes.dbg_pegawaipembagian2_nama}</td>
+                      <td>{data._attributes.dbg_pegawaipembagian3_nama}</td>
+                      <td>{data._attributes.dbg_pegawaipriv}</td>
+                      <td></td>
+                    </tr>
+                  </tbody>
+                ))
+              )}
+            </table>
+          </div>
+        </div>
       </div>
     </section>
   );
